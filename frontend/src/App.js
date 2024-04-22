1afe00736aefdd4e1af7b1ab06dd497b4f29bc65
sachin <b20129@students.iitmandi.ac.in>
2024-04-22 11:27:22 +0530

    Added category option in addExpense and All expense list shows users all added expenses.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,26 @@ import DropdownWithAdd from "./components/dropdown";
 import { useState } from "react";
 
 function App() {
+  const [expenseOptions, setExpenseOptions] = useState([]);
+  const [myExpenses, setMyExpenses] = useState([]);
 
-  const [expenses, setExpenses] = useState([]);
+  const [selectedExpense, setSelectedExpense] = useState([]);
+  const [selectedExpenseCategory, setSelectedExpenseCategory] = useState([]);
+  const [selectedAmount, setSelectedAmount] = useState([]);
+  
   const [salary, setSalary] = useState(1200000);
   const [userId, setUserId] = useState("user123");
 
-  const handleAddExpense = (expense) => {
-    setExpenses([...expenses, expense]);
+  const handleAddExpenseOption = (expense) => {
+    setExpenseOptions([...expenseOptions, expense]);
+  };
+
+  
+
+  const handleAddExpense = (name, amount) => {
+    const newExpense = { name, monthlyAmount: amount };
+    setMyExpenses([...myExpenses, newExpense]);
+    console.log(name,amount);
   };
 
   const expenseTypes = [
@@ -28,59 +41,120 @@ function App() {
     { name: "School & College Expenses", monthlyAmount: 0 },
     { name: "Miscellaneous expenses", monthlyAmount: 0 },
     { name: "Insurance payments", monthlyAmount: 1200 },
-    { name: "Investments", monthlyAmount: 600 }
+    { name: "Investments", monthlyAmount: 600 },
   ];
 
-  const totalMonthlyAmount = expenses.reduce((total, expense) => total + expense.monthlyAmount, 0);
+  const totalMonthlyAmount = expenseOptions.reduce(
+    (total, expense) => total + expense.monthlyAmount,
+    0
+  );
   const percentageOfSalary = (totalMonthlyAmount * 100) / salary;
 
   return (
     <div className="container mx-auto min-h-screen bg-[#000] px-4 py-8 rounded-lg shadow-md">
-
       {/* NAVBAR */}
       <nav className="flex items-center justify-between mb-8 bg-[#EEEEEE] fixed top-0 left-0 right-0 z-10 px-4 py-2">
         <h1 className="text-2xl font-semibold text-black">Expense Tracker</h1>
         <div className="flex items-center">
           <p className="text-black mr-4">{userId}</p>
-          <button className="px-4 py-2 bg-[#4D3C77] text-white rounded-md">Logout</button>
+          <button className="px-4 py-2 bg-[#4D3C77] text-white rounded-md">
+            Logout
+          </button>
         </div>
       </nav>
 
       {/* MAIN CONTENT */}
       <div className="mainContent pt-16 flex flex-col md:flex-row items-center justify-center">
         <div className="expenseAdder bg-[#0F0F0F] rounded-lg p-4 shadow-md mr-0 md:mr-8 mb-4 md:mb-0 w-full md:w-[40%]">
-          <h2 className="text-xl font-semibold text-[#EEEEEE] mb-4">Add Expense</h2>
-          <DropdownWithAdd onAddExpense={handleAddExpense} />
-          <input type="number" placeholder="Amount" className="mt-4 p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-400 block w-full" />
-          <button className="mt-4 px-4 py-2 bg-[#4D3C77] text-white rounded-md block w-full">Add</button>
+          <h2 className="text-xl font-semibold text-[#EEEEEE] mb-4">
+            Add Expense
+          </h2>
+          {/* Expense category */}
+          <select
+            value={selectedExpenseCategory}
+            onChange={(e) => setSelectedExpenseCategory(e.target.value)}
+            className="mb-4 mt-4 p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-400 block w-full"
+          >
+            <option value="">Category</option>
+            {expenseTypes.map((expense, index) => (
+              <option key={index} value={expense.name}>
+                {expense.name}
+              </option>
+            ))}
+          </select>
+
+          {/* Expense adder */}
+          <DropdownWithAdd selectedExpense={selectedExpense} setSelectedExpense={setSelectedExpense} onAddExpense={handleAddExpenseOption}/>
+          <input
+            type="number"
+            placeholder="Amount"
+            className="mt-4 p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-400 block w-full"
+            onChange={(e)=>setSelectedAmount(e.target.value)}
+          />
+          <button
+            onClick={() => handleAddExpense(selectedExpense, selectedAmount)} // Example values, replace with actual data input
+            className="mt-4 px-4 py-2 bg-[#4D3C77] text-white rounded-md block w-full"
+          >
+            Add
+          </button>
         </div>
         <div className="summary bg-[#0F0F0F] rounded-lg p-4 shadow-md mt-4 md:mt-0 flex-grow w-full md:w-[60%]">
           <h2 className="text-xl font-semibold text-[#EEEEEE] mb-4">Summary</h2>
-          {expenseTypes.map((expense, index) => (
-            <div key={index} className="flex justify-between mb-2">
-              <p className="text-sm font-medium text-[#EEEEEE] w-2/6">{expense.name}</p>
-              <p className="text-sm text-[#F2F7A1] w-1/6">{`₹${expense.monthlyAmount.toLocaleString()}`}</p>
-              <p className="text-sm text-[#F2F7A1] w-1/6">{`₹${(expense.monthlyAmount * 12).toLocaleString()}`}</p>
-              <p className="text-sm text-[#F2F7A1] w-1/6">{`${((expense.monthlyAmount * 100) / salary).toFixed(2)}%`}</p>
-            </div>
-          ))}
+          {expenseTypes &&
+            expenseTypes.map((expense, index) => (
+              <div key={index} className="flex justify-between mb-2">
+                <p className="text-sm font-medium text-[#EEEEEE] w-2/6">
+                  {expense.name}
+                </p>
+                <p className="text-sm text-[#F2F7A1] w-1/6">{`₹${expense.monthlyAmount.toLocaleString()}`}</p>
+                <p className="text-sm text-[#F2F7A1] w-1/6">{`₹${(
+                  expense.monthlyAmount * 12
+                ).toLocaleString()}`}</p>
+                <p className="text-sm text-[#F2F7A1] w-1/6">{`${(
+                  (expense.monthlyAmount * 100) /
+                  salary
+                ).toFixed(2)}%`}</p>
+              </div>
+            ))}
           <div className="flex justify-between mt-4">
             <p className="text-sm font-medium text-[#EEEEEE] w-2/6">Total</p>
             <p className="text-sm text-[#F2F7A1] w-1/6">{`₹${totalMonthlyAmount.toLocaleString()}`}</p>
-            <p className="text-sm text-[#F2F7A1] w-1/6">{`₹${(totalMonthlyAmount * 12).toLocaleString()}`}</p>
-            <p className="text-sm text-[#F2F7A1] w-1/6">{`${percentageOfSalary.toFixed(2)}%`}</p>
+            <p className="text-sm text-[#F2F7A1] w-1/6">{`₹${(
+              totalMonthlyAmount * 12
+            ).toLocaleString()}`}</p>
+            <p className="text-sm text-[#F2F7A1] w-1/6">{`${percentageOfSalary.toFixed(
+              2
+            )}%`}</p>
           </div>
           <div className="flex justify-between mt-4">
             <p className="text-sm font-medium text-[#EEEEEE] w-2/6">Savings</p>
-            <p className="text-sm text-[#F2F7A1] w-1/6">{`₹${(salary * 12 - totalMonthlyAmount).toLocaleString()}`}</p>
-            <p className="text-sm text-[#F2F7A1] w-1/6">{`₹${((salary * 12 - totalMonthlyAmount) * 12).toLocaleString()}`}</p>
-            <p className="text-sm text-[#F2F7A1] w-1/6">{`${(100 - percentageOfSalary).toFixed(2)}%`}</p>
+            <p className="text-sm text-[#F2F7A1] w-1/6">{`₹${(
+              salary * 12 -
+              totalMonthlyAmount
+            ).toLocaleString()}`}</p>
+            <p className="text-sm text-[#F2F7A1] w-1/6">{`₹${(
+              (salary * 12 - totalMonthlyAmount) *
+              12
+            ).toLocaleString()}`}</p>
+            <p className="text-sm text-[#F2F7A1] w-1/6">{`${(
+              100 - percentageOfSalary
+            ).toFixed(2)}%`}</p>
           </div>
         </div>
       </div>
       <div className="AllExpenses bg-[#0F0F0F] p-4 shadow-md mt-8">
-        <h2 className="text-xl font-semibold text-[#EEEEEE] mb-4">All Expenses</h2>
-        {/* Add components for displaying all expenses */}
+        <h2 className="text-xl font-semibold text-[#EEEEEE] mb-4">
+          All Expenses
+        </h2>
+        {myExpenses &&
+          myExpenses.map((expense, index) => (
+            <div key={index} className="flex justify-between mb-2">
+              <p className="text-sm font-medium text-[#EEEEEE]">
+                {expense.name}
+              </p>
+              <p className="text-sm text-[#F2F7A1]">{`₹${expense.monthlyAmount.toLocaleString()}`}</p>
+            </div>
+          ))}
       </div>
     </div>
   );
--- frontend/src/components/dropdown.jsx
+++ frontend/src/components/dropdown.jsx
@@ -1,16 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-const DropdownWithAdd = () => {
-  const [selectedOption, setSelectedOption] = useState('');
-  const [customOption, setCustomOption] = useState('');
-  const [options, setOptions] = useState([
-    'milk',
-    'perfumes',
-    'non-veg'
-  ]);
+const DropdownWithAdd = ({ selectedExpense, setSelectedExpense }) => {
+  const [customOption, setCustomOption] = useState("");
+  const [options, setOptions] = useState(["milk", "perfumes", "non-veg"]);
 
   const handleOptionChange = (event) => {
-    setSelectedOption(event.target.value);
+    setSelectedExpense(event.target.value);
+    console.log(event.target.value);
   };
 
   const handleCustomOptionChange = (event) => {
@@ -18,21 +14,26 @@ const DropdownWithAdd = () => {
   };
 
   const handleAddCustomOption = () => {
-    if (customOption.trim() !== '') {
+    if (customOption.trim() !== "") {
       setOptions([...options, customOption]);
-      setCustomOption('');
+      setCustomOption("");
     }
   };
 
   const handleKeyPress = (event) => {
-    if (event.key === 'Enter') {
+    if (event.key === "Enter") {
       handleAddCustomOption();
     }
   };
 
   return (
     <div className="mb-4">
-      <select value={selectedOption} onChange={handleOptionChange} className="p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-400 block w-full">
+      <select
+        value={selectedExpense}
+        onChange={handleOptionChange}
+        className="p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-400 block w-full"
+      >
+        <option value="">Expense</option>
         {options.map((option, index) => (
           <option key={index} value={option}>
             {option}
@@ -40,7 +41,7 @@ const DropdownWithAdd = () => {
         ))}
         <option value="add">Add Custom Option</option>
       </select>
-      {selectedOption === 'add' && (
+      {selectedExpense === "add" && (
         <div className="mt-2 flex items-center">
           <input
             type="text"
@@ -50,7 +51,12 @@ const DropdownWithAdd = () => {
             placeholder="Enter custom option"
             className="p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-400 mr-2 block w-full"
           />
-          <button onClick={handleAddCustomOption} className="px-4 py-2 bg-76ABAE text-white rounded-md">Add</button>
+          <button
+            onClick={handleAddCustomOption}
+            className="px-4 py-2 bg-76ABAE text-white rounded-md"
+          >
+            Add
+          </button>
         </div>
       )}
     </div>
