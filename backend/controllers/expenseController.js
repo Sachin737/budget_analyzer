b0ddf888ea13bdb74174a0c3602268f1576cf5e9
= <=>
2024-04-22 15:10:20 +0530

    generic responses for endpoints in expense model

--- backend/controllers/expenseController.js
+++ backend/controllers/expenseController.js
@@ -1,36 +1,89 @@
 const Expense = require('./../models/expenseModel');
+const AppError = require('../utils/appError');
 
-exports.getAllExpenseDetails = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined!',
-    });
+exports.getAllExpenseDetails = async (req, res, next) => {
+    try {
+        const expenses = await Expense.find();
+        res.status(200).json({
+            status: 'success',
+            results: expenses.length,
+            data: {
+                expenses: expenses,
+            },
+        });
+    } catch (err) {
+        next(err);
+    }
 };
 
-exports.addExpenseDetail = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined!',
-    });
+exports.addExpenseDetail = async (req, res, next) => {
+    try {
+        const expense = await Expense.create(req.body);
+
+        res.status(201).json({
+            status: 'success',
+            data: {
+                data: expense,
+            },
+        });
+    } catch (err) {
+        next(err);
+    }
 };
 
-exports.getExpenseDetail = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined!',
-    });
+exports.getExpenseDetail = async (req, res, next) => {
+    try {
+        const expense = await Expense.findById(req.params.id);
+        res.status(200).json({
+            status: 'success',
+            data: {
+                expense,
+            },
+        });
+    } catch (err) {
+        next(err);
+    }
 };
 
-exports.updateExpenseDetail = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined!',
-    });
+exports.updateExpenseDetail = async (req, res, next) => {
+    try {
+        const expense = await Expense.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {
+                new: true,
+                runValidators: true,
+            }
+        );
+
+        if (!expense) {
+            return next(new AppError('No document found with that ID', 404));
+        }
+
+        res.status(200).json({
+            status: 'success',
+            data: {
+                data: expense,
+            },
+        });
+    } catch (err) {
+        next(err);
+    }
 };
 
-exports.deleteExpenseDetail = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined!',
-    });
+exports.deleteExpenseDetail = async (req, res, next) => {
+    try {
+        const expense = await Expense.findByIdAndDelete(req.params.id);
+
+        if (!expense) {
+            return next(new AppError('No document found with that ID', 404));
+        }
+
+        res.status(204).json({
+            status: 'success',
+            data: null,
+        });
+    } catch (err) {
+        next(err);
+    }
 };
