1e31ba77e9eb083aee03fe203c120799ef5f14d9
Rustam Narayan <b20128@students.iitmandi.ac.in>
2024-04-28 23:15:20 +0530

    user authentication implemented (1/3)

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,7 @@
+const crypto = require('crypto');
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema(
     {
@@ -30,26 +32,26 @@ const userSchema = new mongoose.Schema(
             type: Number,
             default: 0,
         },
-        // password: {
-        //     type: String,
-        //     required: [true, 'Please provide a password'],
-        //     minlength: 8,
-        //     // select: false, // this will make sure that password is not visible in output
-        // },
-        // passwordConfirm: {
-        //     type: String,
-        //     required: [true, 'Please confirm your password'],
-        //     validate: {
-        //         // This only works on CREATE and SAVE!!! (not with update one)
-        //         validator: function (el) {
-        //             return el === this.password;
-        //         },
-        //         message: 'Passwords are not the same!',
-        //     },
-        // },
-        // passwordChangedAt: Date,
-        // passwordResetToken: String,
-        // passwordResetExpires: Date,
+        password: {
+            type: String,
+            required: [true, 'Please provide a password'],
+            minlength: 8,
+            // select: false, // this will make sure that password is not visible in output
+        },
+        passwordConfirm: {
+            type: String,
+            required: [true, 'Please confirm your password'],
+            validate: {
+                // This only works on CREATE and SAVE!!! (not with update one)
+                validator: function (el) {
+                    return el === this.password;
+                },
+                message: 'Passwords are not the same!',
+            },
+        },
+        passwordChangedAt: Date,
+        passwordResetToken: String,
+        passwordResetExpires: Date,
 
         active: {
             type: Boolean,
@@ -62,6 +64,68 @@ const userSchema = new mongoose.Schema(
         toObject: { virtuals: true },
     }
 );
+
+userSchema.pre('save', async function (next) {
+    // Only run this function if password was actually modified
+    if (!this.isModified('password')) return next();
+
+    // Hash the password with cost of 12
+    this.password = await bcrypt.hash(this.password, 12);
+
+    // Delete passwordConfirm field
+    this.passwordConfirm = undefined;
+    next();
+});
+
+userSchema.pre('save', function (next) {
+    if (!this.isModified('password') || this.isNew) return next();
+
+    this.passwordChangedAt = Date.now() - 1000;
+    next();
+});
+
+// Query Middleware
+userSchema.pre(/^find/, function (next) {
+    // this points to the current query
+    this.find({ active: { $ne: false } });
+    next();
+});
+
+userSchema.methods.correctPassword = async function (
+    candidatePassword,
+    userPassword
+) {
+    return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
+    if (this.passwordChangedAt) {
+        const changedTimestamp = parseInt(
+            this.passwordChangedAt.getTime() / 1000,
+            10
+        );
+
+        return JWTTimestamp < changedTimestamp;
+    }
+
+    // False means NOT changed
+    return false;
+};
+
+userSchema.methods.createPasswordResetToken = function () {
+    const resetToken = crypto.randomBytes(32).toString('hex');
+
+    this.passwordResetToken = crypto
+        .createHash('sha256')
+        .update(resetToken)
+        .digest('hex');
+
+    console.log({ resetToken }, this.passwordResetToken);
+
+    this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+
+    return resetToken;
+};
 
 userSchema.virtual('monthlyIncome').get(function () {
     return this.salaryAfterTax + this.otherIncomes;
---
+++ backend/utils/email.js
@@ -0,0 +1,81 @@
+const nodemailer = require('nodemailer');
+const htmlToText = require('html-to-text');
+
+// new Email(user, url).sendWelcome();
+
+module.exports = class Email {
+    constructor(user, url) {
+        this.to = user.email;
+        // this.cc = process.env.EMAIL_CC,
+        this.firstName = user.name.split(' ')[0];
+        this.url = url;
+        this.from = `Natours-Admin <${process.env.EMAIL_FROM}>`;
+    }
+
+    newTransport() {
+        if (process.env.NODE_ENV === 'production') {
+            // used mailsac --> for disposable email
+            // sendinBlue(brevo) instead of sendgrid
+            return nodemailer.createTransport({
+                service: 'SendinBlue',
+                // alternatively we can use smtp host address and port
+                auth: {
+                    user: process.env.BREVO_USERNAME,
+                    pass: process.env.BREVO_API_KEY,
+                },
+            });
+        }
+
+        return nodemailer.createTransport({
+            host: process.env.EMAIL_HOST,
+            port: process.env.EMAIL_PORT,
+            auth: {
+                user: process.env.EMAIL_USERNAME,
+                pass: process.env.EMAIL_PASSWORD,
+            },
+        });
+    }
+
+    // Send the actual email
+    async send(template, subject) {
+        // 1) Render HTML based on a pug template
+        const html = pug.renderFile(
+            `${__dirname}/../views/emails/${template}.pug`,
+            {
+                firstName: this.firstName,
+                url: this.url,
+                subject,
+            }
+        );
+
+        // 2) Define email options
+        const mailOptions = {
+            from: this.from,
+            to: this.to,
+            cc: this.cc,
+            subject,
+            html,
+            text: htmlToText.convert(html),
+        };
+
+        // 3) Create a transport and send email
+        await this.newTransport().sendMail(mailOptions, function (err, data) {
+            if (err) {
+                console.log('Error sending mail 💥', err);
+            } else {
+                console.log('Email sent successfully');
+            }
+        }); // putting a callback function with transporter is optional
+    }
+
+    async sendWelcome() {
+        await this.send('welcome', 'Welcome to the Natours Family!');
+    }
+
+    async sendPasswordReset() {
+        await this.send(
+            'passwordReset',
+            'Your password reset token (valid for only 10 minutes)'
+        );
+    }
+};
